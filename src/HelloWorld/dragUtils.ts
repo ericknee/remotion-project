187de4e75d6402c8f1e7380bde40e36644d483ee
Eric Ni <eni@ucsb.edu>
2024-06-02 05:33:19 -0700

    removed unncecessary files

--- src/HelloWorld/dragUtils.ts
+++
@@ -1,8 +0,0 @@
-export const loadPosition = (key: string) => {
-    const savedPosition = localStorage.getItem(key);
-    return savedPosition ? JSON.parse(savedPosition) : { x: 0, y: 0 };
-  };
-  
-  export const savePosition = (key: string, position: { x: number, y: number }) => {
-    localStorage.setItem(key, JSON.stringify(position));
-  };
